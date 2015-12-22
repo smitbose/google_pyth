e236e14e20337c7a52f6e525d9627120a38d7acf
= <bosmit90@gmail.com>
2015-12-23 01:22:47 +0530

    Added os and high level operations assignment

---
+++ copyspecial/copyspecial.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+import sys
+import re
+import os
+import shutil
+import commands
+
+"""Copy Special exercise
+"""
+
+# +++your code here+++
+# Write functions and modify main() to call them
+
+#this function returns the special files in the given directory
+def get_files(dir):
+  files = os.listdir(dir)
+  special_files = []
+  for file in files:
+    match = re.search(r'__\w+__',file)
+    if match:
+      special_files.append(file)
+  return special_files
+
+
+
+
+def main():
+  # This basic command line argument parsing code is provided.
+  # Add code to call your functions below.
+
+  # Make a list of command line arguments, omitting the [0] element
+  # which is the script itself.
+  args = sys.argv[1:]
+  if not args:
+    print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
+    sys.exit(1)
+
+  # todir and tozip are either set from command line
+  # or left as the empty string.
+  # The args array is left just containing the dirs.
+  todir = ''
+  if args[0] == '--todir':
+    todir = args[1]
+    del args[0:2]
+
+  tozip = ''
+  if args[0] == '--tozip':
+    tozip = args[1]
+    del args[0:2]
+
+  if len(args) == 0:
+    print "error: must specify one or more dirs"
+    sys.exit(1)
+
+  # +++your code here+++
+  # Call your functions
+  abstodir = ''
+  if todir != '':
+    abstodir = os.path.abspath(todir)
+    if os.path.exists(abstodir) == False:
+      os.mkdir(abstodir)
+
+  for dir in args:
+    absdir = os.path.abspath(dir)
+    listfiles = get_files(absdir)
+    zipcommand = 'zip -j ' + tozip
+    for files in listfiles:
+      print files
+      if todir != '':
+        shutil.copy(os.path.join(absdir,files),os.path.join(abstodir,files))
+      if tozip != '':
+        zipcommand+=' '+os.path.join(absdir,files)
+    if tozip != '':
+      os.system(zipcommand)
+
+
+  sys.exit(0)
+
+if __name__ == "__main__":
+  main()
